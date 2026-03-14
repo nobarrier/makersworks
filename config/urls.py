46623a150f3a0d16de353a26c5df9045b410feac
Books <mybookseeker@gmail.com>
2026-03-14 22:26:36 +0900

    flatten django config structure

--- config/urls.py
+++ config/urls.py
@@ -1,15 +1,16 @@
 from django.contrib import admin
-from django.shortcuts import redirect
 from django.urls import include, path
+from django.shortcuts import redirect
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
-    path("", lambda request: redirect("catalog/")),
+    path("", RedirectView.as_view(url="/catalog/", permanent=False)),
     path("admin/", admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),
-    path("users/", include("users.urls")),
     path("accounts/custom/", include("accounts.urls")),
+    path("users/", include("users.urls")),
     path("catalog/", include("apps.catalog.urls")),
     path("cart/", include("apps.cart.urls")),
     path("order/", include("apps.orders.urls")),
